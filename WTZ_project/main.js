215e49e25440f478a16fe3a00c56429ba87ecfbe
Alexander Bychkov <51967476+alxbychkov@users.noreply.github.com>
2020-03-16 01:21:44 +0300

    changes in main

--- WTZ_project/main.js
+++ WTZ_project/main.js
@@ -5,61 +5,59 @@ var xhr = new XMLHttpRequest(),
     page = 0,
     elements = [];
 
-document.addEventListener('scroll', () => {
-    if (nextPage()) {
-        render(API_KEY, page);
-    }
-});
-
-function nextPage() {
-    var containerHeight = document.querySelector('.container').offsetHeight,
-        screenHeight = window.outerHeight,
-        screenScroll = window.pageYOffset;
-    if ((containerHeight - screenHeight) < screenScroll) {
-        page++;
-        return true;
-    }
-}
-
-function render(API_KEY, page) {
-    url = `https://api.unsplash.com/photos?page=${page}&client_id=${API_KEY}`;
-
-    xhr.open('get', url, false);
-    xhr.onload = function () {
-        if (xhr.readyState == 4 && (~~(xhr.status / 100)) == 2) {
-            data = JSON.parse(xhr.responseText);
-            let ln = data.length;
-            for (let i = 0; i < ln; i++) {
-                elements.push({
-                    href: data[i].user.links.html,
-                    name: data[i].user.name,
-                    username: '@' + data[i].user.username,
-                    avatar: data[i].user.profile_image.small,
-                    photo: data[i].urls.small,
-                    likes: data[i].likes,
-                });
-            }
-        }
-    }
-    xhr.send(null);
-}
-
-render(API_KEY, page);
-
 var app = new Vue({
     el: '#app',
     data: {
         elements
     },
-    computed: {
-        now: function () {
-            return Date.now()
-        }
-    },
     methods: {
         cur_page: function () {
             return this.elements.length / 10;
         },
+        render(API_KEY, page) {
+            url = `https://api.unsplash.com/photos?page=${page}&client_id=${API_KEY}`;
+
+            xhr.open('get', url, false);
+            xhr.onload = function () {
+                if (xhr.readyState == 4 && (~~(xhr.status / 100)) == 2) {
+                    data = JSON.parse(xhr.responseText);
+                    let ln = data.length;
+                    for (let i = 0; i < ln; i++) {
+                        elements.push({
+                            href: data[i].user.links.html,
+                            name: data[i].user.name,
+                            username: '@' + data[i].user.username,
+                            avatar: data[i].user.profile_image.small,
+                            photo: data[i].urls.small,
+                            likes: data[i].likes,
+                        });
+                    }
+                }
+            }
+            xhr.send(null);
+        },
+        nextPage() {
+            var containerHeight = document.querySelector('.container').offsetHeight,
+                screenHeight = window.outerHeight,
+                screenScroll = window.pageYOffset;
+            if ((containerHeight - screenHeight) < screenScroll) {
+                page++;
+                return true;
+            }
+        },
+        scroll() {
+            document.addEventListener('scroll', () => {
+                if (this.nextPage()) {
+                    this.render(API_KEY, page);
+                }
+            });
+        }
+    },
+    beforeMount() {
+        this.render(API_KEY, page);
+    },
+    mounted() {
+        this.scroll();
     }
 })
 
