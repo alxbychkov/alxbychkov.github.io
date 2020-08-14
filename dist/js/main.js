960132f9d7d1268cef7143f26d6ed6fba9841006
Alexander Bychkov <51967476+alxbychkov@users.noreply.github.com>
2020-08-14 18:28:58 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,7 @@ document.querySelector(".first-screen .navigation").addEventListener('wheel', fu
 });
 document.addEventListener('DOMContentLoaded', function () {
   var objs = document.querySelector('.first-screen');
-  showObj(objs, 2000);
+  showObj(objs, 0);
 });
 
 function showObj(name, time) {
@@ -35,13 +35,69 @@ document.addEventListener('scroll', function (e) {
   var scroll = document.documentElement.scrollTop + window.innerHeight;
 
   if (scroll > portfolio.offsetTop) {
-    showObj(portfolio, 600);
+    showObj(portfolio, 0);
   }
 
   if (scroll > advantages.offsetTop) {
-    showObj(advantages, 2000);
+    showObj(advantages, 0);
   }
-});
+}); // preloader()
+
+function preloader() {
+  var images = document.images,
+      images_total_count = images.length,
+      preloader = document.querySelector('.preloader'),
+      perc_display = preloader.querySelectorAll('.preloader_percentage .perc');
+  var images_loaded_count = 0;
+
+  for (var i = 0; i < images_total_count; i++) {
+    var image_clone = new Image();
+    image_clone.onload = image_loaded;
+    image_clone.onerror = image_loaded;
+    image_clone.src = images[i].src;
+  }
+
+  function image_loaded() {
+    images_loaded_count++;
+    perc_display.innerHTML = "".concat(100 / images_loaded_count * images_loaded_count << 0, "%");
+
+    if (images_loaded_count >= images_total_count) {
+      setTimeout(function () {// !preloader.classList.contains('done') && preloader.classList.add('done')
+      }, 1000);
+    }
+  }
+
+  move();
+
+  function move() {
+    var back = document.querySelector('.preloader_back');
+    var width = 1;
+    var id = setInterval(function () {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++;
+        back.style.width = "".concat(width, "%");
+        perc_display.forEach(function (display) {
+          display.innerHTML = addZero(width);
+        });
+      }
+    }, 40);
+  }
+}
+
+function addZero(number) {
+  var perc = Math.trunc(number / 10);
+  if (perc > 0 && perc < 10) return "0".concat(number);
+
+  switch (perc) {
+    case 0:
+      return "00".concat(number);
+
+    case 10:
+      return number;
+  }
+}
 
 function testWebP(callback) {
   var webP = new Image();
